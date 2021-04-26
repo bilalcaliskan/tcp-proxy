e9938e66c370e9a380a1f0d28a2b6791d22b17e5
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-04-27 00:45:40 +0300

    remove failed tests

--- pkg/logging/logging_test.go
+++ pkg/logging/logging_test.go
@@ -8,6 +8,6 @@ import (
 func TestCreateLogger(t *testing.T) {
 	_, err := zap.NewProduction()
 	if err != nil {
-		t.Fatalf("an error occured while creating logger, error is %v\n", err.Error())
+		t.Errorf("%v\n", err.Error())
 	}
 }
