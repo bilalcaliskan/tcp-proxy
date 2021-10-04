72fda8271d8a9f2fe988a35d978e120c72971b7a
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-10-05 01:28:42 +0300

    assign opts to vars

--- internal/options/options_test.go
+++ internal/options/options_test.go
@@ -3,5 +3,7 @@ package options
 import "testing"
 
 func TestGetTcpProxyOptions(t *testing.T) {
-	t.Logf("fetched default TcpProxyOptions, %v\n", GetTcpProxyOptions())
+	t.Log("fetching default options.TcpProxyOptions")
+	opts := GetTcpProxyOptions()
+	t.Logf("fetched default options.TcpProxyOptions, %v\n", opts)
 }
