74dfee88b41d5c718b3e2140ffeab2c77386f3c5
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-01-04 00:05:57 +0300

    increase converage

--- internal/options/options_test.go
+++ internal/options/options_test.go
@@ -1,9 +1,13 @@
 package options
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestGetTcpProxyOptions(t *testing.T) {
 	t.Log("fetching default options.TcpProxyOptions")
 	opts := GetTcpProxyOptions()
+	assert.NotNil(t, opts)
 	t.Logf("fetched default options.TcpProxyOptions, %v\n", opts)
 }
--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -1,8 +1,12 @@
 package proxy
 
 import (
+	"crypto/tls"
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net"
+	"net/http"
 	"sync"
 	"testing"
 	"time"
@@ -12,31 +16,45 @@ import (
 // https://ipfs.io/ipfs/QmfYeDhGH9bZzihBUDEQbCbTc5k5FZKURMUoUvfmc27BwL/socket/tcp_sockets.html
 func TestProxy(t *testing.T) {
 	var cases = []struct {
-		name, proxyProto, targetProto, targetDns string
-		proxyPort, targetPort                    int
+		name, proxyProto, targetProto, targetDns, contentUrl string
+		proxyPort, targetPort                                int
 	}{
-		{"TCP3000", "tcp", "tcp", "en.wikipedia.org", 3000, 443},
-		// {"TCP3001", "tcp", "tcp", "en.wikipedia.org", 3001, 443},
+		{"TCP3000", "tcp", "tcp", "en.wikipedia.org",
+			"/wiki/OSI_model", 3000, 443},
+		{"TCP3001", "tcp", "tcp", "en.wikipedia.org",
+			"/wiki/Tesla,_Inc.", 3001, 443},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-
 			go func() {
+				var conn net.Conn
+				var err error
+
+				defer func(conn net.Conn) {
+					err := conn.Close()
+					if err != nil {
+						t.Error("an error occured while closing current connection")
+						return
+					}
+				}(conn)
+
 				listener, err := net.Listen(tc.proxyProto, fmt.Sprintf(":%d", tc.proxyPort))
 				if err != nil {
 					t.Errorf("%v\n", err.Error())
 					return
 				}
 
-				conn, err := listener.Accept()
-				if err != nil {
-					t.Errorf("%v\n", err.Error())
-					return
-				}
+				for {
+					conn, err = listener.Accept()
+					if err != nil {
+						t.Errorf("%v\n", err.Error())
+						return
+					}
 
-				connectionStr := fmt.Sprintf("%s:%d", tc.targetDns, tc.targetPort)
-				go Proxy(conn, tc.targetProto, connectionStr)
+					connectionStr := fmt.Sprintf("%s:%d", tc.targetDns, tc.targetPort)
+					go Proxy(conn, tc.targetProto, connectionStr)
+				}
 			}()
 
 			var wg sync.WaitGroup
@@ -61,10 +79,30 @@ func TestProxy(t *testing.T) {
 					}
 
 					t.Logf("connection to port %d succeeded!\n", tc.proxyPort)
+
+					// make http request and see response
+					client := &http.Client{
+						Timeout: 5 * time.Second,
+						Transport: &http.Transport{
+							TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+						},
+					}
+					req, err := http.NewRequest("GET", fmt.Sprintf("https://127.0.0.1:%d%s", tc.proxyPort,
+						tc.contentUrl), nil)
+					assert.Nil(t, err)
+
+					req.Host = tc.targetDns
+					resp, err := client.Do(req)
+					assert.Nil(t, err)
+
+					body, err := ioutil.ReadAll(resp.Body)
+					assert.Nil(t, err)
+					assert.NotEmpty(t, string(body))
+
 					break
 				}
 
-				defer wg.Done()
+				wg.Done()
 			}()
 
 			wg.Wait()
